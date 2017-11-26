cadcc5ff5d9a884fc6ac3fb3a64af32654cee080
Akansh <akansh.gulati@wingify.com>
2017-11-27 02:17:18 +0530

    fix(todo-better): Refined

--- src/app.vue
+++ src/app.vue
@@ -14,12 +14,10 @@
                         <div class="flex flex-center">
                             <div class="todo-widget relative" v-on:keydown.stop="">
                                 <div class="todo-icon pointer" v-on:click.stop="toggleTodos">
-                                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 512 512"
-                                         enable-background="new 0 0 512 512" width="1.8rem">
+                                    <svg viewBox="0 0 512 512" enable-background="new 0 0 512 512" width="1.8rem">
                                         <g>
                                             <g id="todo_btn">
-                                                <path d="M370,95.4V53.9h-62.2V12.5H204.2v41.5H142v41.5H69.5v404.1h373.1V95.4H370z M224.9,33.2h62.2v20.7h-62.2V33.2z     M162.7,74.7h186.5v62.2H162.7V74.7z M421.8,478.8H90.2V116.1H142v41.4h228v-41.4h51.8V478.8z"
-                                                      fill="#FFFFFF"/>
+                                                <path d="M370,95.4V53.9h-62.2V12.5H204.2v41.5H142v41.5H69.5v404.1h373.1V95.4H370z M224.9,33.2h62.2v20.7h-62.2V33.2z M162.7,74.7h186.5v62.2H162.7V74.7z M421.8,478.8H90.2V116.1H142v41.4h228v-41.4h51.8V478.8z"/>
                                                 <rect width="20.7" x="131.6" y="222.8" height="20.7"/>
                                                 <rect width="20.7" x="131.6" y="307.8" height="20.7"/>
                                                 <rect width="20.7" x="131.6" y="393.8" height="20.7"/>
--- src/utils/timeUtil.js
+++ src/utils/timeUtil.js
@@ -1,9 +1,19 @@
 export default {
     isTodayDate(timeStamp){
+        if(!timeStamp){
+            return false;
+        }
+        timeStamp = +new Date(timeStamp);
         // This will be according to user time zone
         let now = new Date();
         let minTime = +now.setHours(0, 0, 0, 0);
         let maxTime = +now.setHours(23, 59, 59, 59);
-        return minTime <= timeStamp <= maxTime;
+        return minTime <= timeStamp && timeStamp <= maxTime;
+    },
+    getEndOfDay(days){
+        let now = new Date().setHours(23,59,59,59);
+        days = days ? days : 0;
+        now = new Date(+now + 86400000 * days);
+        return now.toISOString().substring(0,10);
     }
 }
