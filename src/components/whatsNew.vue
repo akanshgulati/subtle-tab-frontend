924b650c1d567c8ef2d1b10f24bea3357f686134
Akansh <akansh.gulati@wingify.com>
2018-01-06 02:32:30 +0530

    feat(whatsnew): Add whatsnew feature

--- src/components/whatsNew.vue
+++ src/components/whatsNew.vue
@@ -1,18 +1,25 @@
 <template>
     <div>
-        <div v-for="data in updates" :key="data.time">
-            <div class="flex flex-justify-space-between mb-10">
-                <h4 class="font-medium bold text-green">v{{data.version}}</h4>
-                <h4 class="font-small ">{{data.time | date}}</h4>
+        <div v-if="!isLoading" class="fade_in">
+            <small class="semi-bold">*Versions not shown are minor fixes and improvements</small>
+            <div v-for="data in updates" :key="data.version">
+                <div class="flex flex-justify-space-between mb-10">
+                    <h4 class="font-medium bold text-green">v{{data.version}}</h4>
+                    <h4 class="font-small ">{{data.time | date}}</h4>
+                </div>
+                <ul class="pl-10 update-list" style="list-style: disc">
+                    <li v-for="log in data.updates" class="font-small pl-10 mb-10">
+                        <p class="semi-bold mar-0 font-medium">{{log.title}}</p>
+                        <p class="mar-0">
+                            {{log.desc}}
+                        </p>
+                    </li>
+                </ul>
             </div>
-            <ul class="pl-10 update-list" style="list-style: disc">
-                <li v-for="log in data.updates" class="font-small pl-10 mb-10">
-                    <p class="semi-bold mar-0 font-medium">{{log.title}}</p>
-                    <p class="mar-0">
-                        {{log.desc}}
-                    </p>
-                </li>
-            </ul>
+        </div>
+        <div v-if="isLoading" class="flex flex-flow-column full-height flex-center flex-justify-center">
+            <img src="/images/loading.svg" alt="loading" width="100px">
+            <p class="bold font-medium">Loading...</p>
         </div>
         <!-- <div v-if="update.improvements" class="pl-10">
                 <p class="text-green mar-0 font-small">Improvement</p>
@@ -28,7 +35,6 @@
     </div>
 </template>
 <script>
-    import bgData from '../utils/backgroundData'
     import storage from '../utils/storage'
     import constants from '../utils/Constants'
     import commonUtil from '../utils/common'
